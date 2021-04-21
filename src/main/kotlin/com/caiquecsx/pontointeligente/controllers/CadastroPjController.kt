d811a0cf3b99b101c4ceb8deebcce4b668fc581b
Caique <caique.c.s@outlook.com>
2021-04-21 17:56:05 -0300

    creating controllers

---
+++ src/main/kotlin/com/caiquecsx/pontointeligente/controllers/CadastroPjController.kt
@@ -0,0 +1,24 @@
+package com.caiquecsx.pontointeligente.controllers
+
+import com.caiquecsx.pontointeligente.dtos.CadastroPJDto
+import com.caiquecsx.pontointeligente.services.EmpresaService
+import com.caiquecsx.pontointeligente.services.FuncionarioService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping
+class CadastroPjController(
+    val empresaService: EmpresaService,
+    val funcionarioService: FuncionarioService
+) {
+
+    @PostMapping
+    fun cadastrar(@Valid @RequestBody cadastroPJDto: CadastroPJDto){
+
+    }
+
+}
---
+++ src/test/kotlin/com/caiquecsx/pontointeligente/controllers/LancamentoControllerTest.kt
@@ -0,0 +1,8 @@
+package com.caiquecsx.pontointeligente.controllers
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LancamentoControllerTest {
+
+}
